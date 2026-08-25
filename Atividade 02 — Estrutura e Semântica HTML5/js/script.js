df4820880d7a017ec6792c20a82f613762fe7ca4
isaac-cms <isaac0410morais@gmail.com>
2026-08-25 09:40:46 -0300

    chore: remove duplicidade da atividade 02

--- Atividade 02 — Estrutura e Semântica HTML5/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('form');
-  if (form) {
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      alert('Planejamento registrado com sucesso!');
-    });
-  }
-});
